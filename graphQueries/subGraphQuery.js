00483099a04ba3373f88503286a9e4c8f17043ac
Ujjwal <vinze77@gmail.com>
2023-03-05 21:32:29 +0530

    Second commit

--- graphQueries/subGraphQuery.js
+++ graphQueries/subGraphQuery.js
@@ -74,4 +74,9 @@ async function main() {
     // });
 }
 
-main();
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
